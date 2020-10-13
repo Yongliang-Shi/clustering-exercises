6132d33b102248ed40d810160e0945e0144892f7
Yongliang Shi <yongliang.michael.shi@gmail.com>
2020-10-13 11:54:27 -0500

    add functions handle_missing_values and drop_remaining_missings

--- prepare.py
+++ prepare.py
@@ -6,11 +6,37 @@ import pandas as pd
 
 import acquire
 
-
+# %%
 def clean_zillow(zillow):
     zillow.drop_duplicates(subset=['parcelid'], keep='last', inplace=True, ignore_index=True)
     return zillow
 
+# %%
+def handle_missing_values(df, prop_required_column, prop_required_row):
+    """
+    Drop rows and columsn based on the perent of values that are missing.
+    Parameters: 
+    1. df
+    2. the proportion, for each column, of rows with non-missing values requied to keep the column
+    3. the proportion, for each row, of columns with non-missing values required to keep the row
+    """
+    threshold = int(round(prop_required_column*len(df.index),0))
+    df.dropna(axis=1, thresh=threshold, inplace=True)
+    threshold = int(round(prop_required_row*len(df.columns),0))
+    df.dropna(axis=0, thresh=threshold, inplace=True)
+    return df
+
+# %%
+def drop_remaining_missings(df):
+    """
+    Drop all the reamining missing values
+    Parameter: df
+    """
+    mask = (df.isna().sum(axis=1) == 0)
+    df = df[mask]
+    return df
+
+# %%
 def scale(train, validate, test, columns_to_scale):
     """
     Scale the columns using MinMaxScaler
