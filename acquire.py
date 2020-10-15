1b411afe90ac06a3f58f1028c512cf6d67aca475
Yongliang Shi <yongliang.michael.shi@gmail.com>
2020-10-15 12:11:18 -0500

    add new function get_zillow_clustering

--- acquire.py
+++ acquire.py
@@ -38,6 +38,31 @@ def get_zillow_data(query, iteration):
         df.to_csv(filename)
         return df
 
+def get_zillow_clustering():
+    filename = 'zillow.csv'
+    query = """
+        select *
+        from properties_2017
+        join predictions_2017 using(parcelid)
+        left join airconditioningtype using(airconditioningtypeid)
+        left join architecturalstyletype using(architecturalstyletypeid)
+        left join buildingclasstype using(buildingclasstypeid)
+        left join heatingorsystemtype using(heatingorsystemtypeid)
+        left join propertylandusetype using(propertylandusetypeid)
+        left join storytype using(storytypeid)
+        left join typeconstructiontype using(typeconstructiontypeid)
+        where transactiondate between '2017-01-01' and '2017-12-31'
+        and latitude is not null
+        and longitude is not null
+        order by parcelid, transactiondate
+        """    
+    if os.path.isfile(filename):
+        return pd.read_csv(filename, index_col=0)
+    else: 
+        df = pd.read_sql(query, get_connection('zillow'))
+        df.to_csv(filename)
+        return df
+
 # %%
 def get_mall_data():
     """
